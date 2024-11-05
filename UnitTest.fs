bf19dec765f19cf8a6d9c102e64052077fb47cee
snOlga <olga.safonova@outlook.com>
2024-11-05 16:51:30 +0300

    first using test!

--- UnitTest.fs
+++ UnitTest.fs
@@ -65,3 +65,26 @@ let Test3 () =
     let actual = newtonInterpolation (Seq.zip x y) 0.00 1.00
     let expected = seq {(0.0, 0.0); (1.0, 2); (2.0, 3.2); (3.0, 4.05); (4.0, 5.0); (5.0, 6.5); (6.0, 9.0)}
     Assert.True(compareResult expected actual)
+
+[<Test>]
+let UsingTest () =
+    let firstPoint = (0.00, 0.00)
+    let secondPoint = (1.571, 1.00)
+
+    let seqCreate someSeq point = Seq.append someSeq (Seq.singleton point)
+    let result someSeq = linearInterpolation (Seq.pairwise someSeq) (fst (Seq.head someSeq)) 1
+
+    let firstSeq = seqCreate (Seq.singleton firstPoint) secondPoint
+    let firstRes = result firstSeq
+
+    let expected = seq {(0.0, 0.0); (1.0, 0.64); (2.0, 1.27)}
+    Assert.True(compareResult expected firstRes)
+
+    let oneMorePoint = (3.142, 0.00)
+    let oneMoreSeq = seqCreate firstSeq oneMorePoint
+    let oneMoreRes = result (Seq.tail oneMoreSeq)
+
+    let expected = seq {(1.57, 1.00); (2.57, 0.36); (3.57, -0.27)}
+    Assert.True(compareResult expected oneMoreRes)
+    
+    
