f399b20b28f8b99b5fc97557b1e428d2283eeb7a
snOlga <olga.safonova@outlook.com>
2024-11-14 19:59:00 +0300

    more abstraction to main

--- Program.fs
+++ Program.fs
@@ -4,31 +4,44 @@ open System
 open System.IO
 open Interpolation
 
+type ApproxState =
+    { Name: string
+      Points: (float * float) seq
+      Step: float
+      Func: (float * float) seq -> float -> float -> (float * float) seq }
+
 let readPoint () =
     let point = Console.ReadLine().Split(" ")
     let x = point[0] |> float
     let y = point[1] |> float
     (x, y)
 
-let seqCreate someSeq point = Seq.append someSeq (Seq.singleton point)
+let seqCreate someSeq point =
+    Seq.append someSeq (Seq.singleton point)
 
 let culculateAndPrint approxName someSeq step approx =
     let result = approx someSeq (fst (Seq.head someSeq)) step
     printfn "%s\n" approxName
+
     for pair in result do
         printfn "%A" pair
 
-let rec doWhileRecursively step currentPointsForLinear currentPointsForNewton =
-    culculateAndPrint "Linear" currentPointsForLinear step linearInterpolation
-    culculateAndPrint "Newton" currentPointsForNewton step newtonInterpolation
-    
+let rec doWhileRecursively (states: seq<ApproxState>) =
+    for state in states do
+        culculateAndPrint state.Name state.Points state.Step state.Func
+
     let newPoint = readPoint ()
-    let newSeqForLinear= seqCreate currentPointsForLinear newPoint
-    let newSeqForNewton= seqCreate currentPointsForNewton newPoint
-    doWhileRecursively step (Seq.tail newSeqForLinear) (Seq.tail newSeqForNewton)
+
+    let newStates =
+        seq {
+            for state in states do
+                yield { state with Points = (seqCreate (Seq.tail state.Points) (newPoint)) }
+        }
+
+    doWhileRecursively newStates
 
 [<EntryPoint>]
-let main (args) = 
+let main (args) =
     match Array.length args with
     | 0 ->
         printf "Step is: "
@@ -36,26 +49,42 @@ let main (args) =
 
         let firstPoint = readPoint ()
         let secondPoint = readPoint ()
-
         let someSeq = seqCreate (Seq.singleton firstPoint) secondPoint
-        //let linearResult = resultLinear someSeq step
 
-        //printfn "\nLinear:\n%A" linearResult
+        let result = linearInterpolation someSeq (fst (Seq.head someSeq)) step
+        printfn "%s\n" "Linear"
+
+        for pair in result do
+            printfn "%A" pair
 
         let newPoint = readPoint ()
-        let newSeqForLinear= seqCreate someSeq newPoint
-        let newSeqForNewton= seqCreate someSeq newPoint
-        doWhileRecursively step (Seq.tail newSeqForLinear) newSeqForNewton
-    // | 1 -> 
-    //     let pointsStr = File.ReadAllLines(args[0])
-    //     let points = Seq.ofArray (Array.map (fun (str: string) -> ((str.Split(" ")[0] |> float), (str.Split(" ")[1] |> float))) (Array.tail pointsStr))
-    //     let step = (Array.head pointsStr) |> float
-    //     let linearResult = resultLinear points step
-    //     let newtonResult = resultNewton points step
-    //     printfn "Linear:\n"
-    //     for pair in linearResult do
-    //         printfn "%A" pair
-    //     printfn "Newton:\n"
-    //     for pair in newtonResult do
-    //         printfn "%A" pair
+
+        let linear =
+            { Name = "Linear"
+              Points = Seq.tail (seqCreate someSeq newPoint)
+              Step = step
+              Func = linearInterpolation }
+
+        let newton =
+            { Name = "Newton"
+              Points = seqCreate someSeq newPoint
+              Step = step
+              Func = newtonInterpolation }
+
+        doWhileRecursively (Seq.append (Seq.singleton linear) (Seq.singleton newton))
+    | 1 ->
+        let pointsStr = File.ReadAllLines(args[0])
+
+        let points =
+            Seq.ofArray (
+                Array.map
+                    (fun (str: string) -> ((str.Split(" ")[0] |> float), (str.Split(" ")[1] |> float)))
+                    (Array.tail pointsStr)
+            )
+
+        let step = (Array.head pointsStr) |> float
+
+        culculateAndPrint "Linear" points step linearInterpolation
+        culculateAndPrint "Newton" points step newtonInterpolation
+
     0
